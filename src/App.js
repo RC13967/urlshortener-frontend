2e4fcbe8863c9a7d4b679a1bbc42ddb07599cc96
ranjith <ranjithch137@gmail.com>
2021-10-19 10:01:04 +0530

    waiting

--- src/App.js
+++ src/App.js
@@ -555,17 +555,15 @@ function RedirectToLong() {
     // eslint-disable-next-line
   }, []);
   return(
-    <>
-    <div>Please wait until we redirect you to the URL</div>
+    <Container className = "container">
+    <div className="home-content">Please wait until we redirect you to the URL</div>
+    <div className="home-header">
     <Spinner animation="grow" variant="primary" />
     <Spinner animation="grow" variant="secondary" />
     <Spinner animation="grow" variant="success" />
     <Spinner animation="grow" variant="danger" />
-    <Spinner animation="grow" variant="warning" />
-    <Spinner animation="grow" variant="info" />
-    <Spinner animation="grow" variant="light" />
-    <Spinner animation="grow" variant="dark" />
-  </>
+    </div>
+  </Container>
   )
 }
 function UrlTable() {
